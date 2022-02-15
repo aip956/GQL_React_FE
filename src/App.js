2569f8dd55bbabed5010bc77e9a011f86984b409
Anthea Ip <antheaip964@gmail.com>
2022-02-14 21:24:43 -0500

    First commit, working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from '@apollo/client';
+import { getAll } from "./GraphQL/Query";
 
 function App() {
+  const {loading,error,data}=useQuery(getAll)
+  if (loading) return "Loading"
+  if (error) return "Error"
+  if(data) console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.getAll.map((data) => (
+
+          <p key={data.title}>
+            {data.title}----{data.description}
+          </p>
+        ))
+      }
     </div>
-  );
+  )
 }
 
 export default App;
